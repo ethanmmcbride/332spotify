eeac274528e4cb8f1d159df84e6174baafb0eba0
Ethan McBride <ethanmmcbride@gmail.com>
2025-04-26 22:26:16 -0700

    display_artists.php works with new DB and clicking a row sends you back to index.php

--- display_artists.php
+++ display_artists.php
@@ -1,22 +1,27 @@
 <h2>Artist List</h2>
 <table>
     <tr>
-        <th>Name</th>
-        <th>Genre</th>
+        <th>Artist Name</th>
+        <th>Streams</th>
+        <th>Features</th>
+        <th>Tracks</th>
     </tr>
     <?php
-    $sql = "SELECT name, genre FROM artists";
+    $sql = "SELECT `Artist Name`, `Lead Streams`, `Feats`, `Tracks`, `One Billion`, `100 Million`, `Last Updated` 
+            FROM spotify_artist_data";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>" . htmlspecialchars($row['name']) . "</td>
-                    <td>" . htmlspecialchars($row['genre']) . "</td>
+            echo "<tr style='cursor: pointer;' onclick=\"window.location='index.php'\">
+                    <td>" . htmlspecialchars($row['Artist Name']) . "</td>
+                    <td>" . htmlspecialchars($row['Lead Streams']) . "</td>
+                    <td>" . htmlspecialchars($row['Feats']) . "</td>
+                    <td>" . htmlspecialchars($row['Tracks']) . "</td>
                   </tr>";
         }
     } else {
-        echo "<tr><td colspan='2'>No artists found</td></tr>";
+        echo "<tr><td colspan='7'>No artists found</td></tr>";
     }
     ?>
 </table>
